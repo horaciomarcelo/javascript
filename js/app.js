a2ca4f123e01ca09a6750bbe2eab06bacab75dac
horaciomarcelo <horaciommarcelo@gmail.com>
2025-10-13 21:33:08 -0300

    Inicializacion del proyecto JS

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+let indice = 0;
+while (indice < 5 ){
+    console.log ("vuelta :" , indice );
+    indice++;
+}
+
+let frutas = ['manzana','banana','babana'];
+for (let fruta of frutas){
+    console.log('fruta : ', fruta);
+}
